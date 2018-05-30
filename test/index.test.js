a030b07dd549802dd160e92d3b6cb73f115d8a84
Yoshiyuki <32473622+yoshwata@users.noreply.github.com>
2018-05-31 03:36:29 +0900

    fix(854): Use pipeline.admin function (#25)

--- test/index.test.js
+++ test/index.test.js
@@ -276,6 +276,12 @@ describe('index test', () => {
 
             executor.tokenGen = tokenGen;
 
+            const adminMock = Promise.resolve({
+                username: 'test'
+            });
+
+            testDelayedConfig.pipeline.admin = adminMock;
+
             executor.startPeriodic(testDelayedConfig).then(() => {
                 assert.calledOnce(queueMock.connect);
                 assert.notCalled(redisMock.hset);
