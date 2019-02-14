f1d196fa3d18fc02fbf5dc131635ce7be6430b97
Pranav Ravichandran <pranavrc@yahoo-inc.com>
2019-02-14 15:40:27 -0800

    fix: Pass jobName to postBuildEvent (#42)
    * fix: Pass jobName to postBuildEvent
    
    * fix: Minor edit

--- index.js
+++ index.js
@@ -318,7 +318,15 @@ class ExecutorQueue extends Executor {
      * @return {Promise}
      */
     async _startFrozen(config) {
-        return this.postBuildEvent(config)
+        const newConfig = {
+            job: {
+                name: config.jobName
+            }
+        };
+
+        Object.assign(newConfig, config);
+
+        return this.postBuildEvent(newConfig)
             .catch(() => Promise.resolve());
     }
 
@@ -350,6 +358,7 @@ class ExecutorQueue extends Executor {
      * @param  {Array}  config.freezeWindows Array of cron expressions that this job cannot run during
      * @param  {String} config.apiUri        Screwdriver's API
      * @param  {String} config.jobId         JobID that this build belongs to
+     * @param  {String} config.jobName       Name of job that this build belongs to
      * @param  {String} config.buildId       Unique ID for a build
      * @param  {Object} config.pipeline      Pipeline of the job
      * @param  {Fn}     config.tokenGen      Function to generate JWT from username, scope and scmContext
